91069b074896b3c3cc738a7ee81f06db53982515
MislavJaksic <jaksicmislav@gmail.com>
2021-06-23 23:07:28 +0200

    Extracted test gen

--- rapdevpy/runner.py
+++ rapdevpy/runner.py
@@ -7,22 +7,3 @@
     :copyrgiht: 2019 MislavJaksic
     :license: MIT License
 """
-import sys
-
-from rapdevpy import pytest_gen
-
-
-def main(args):
-    """main() will be run if you run this script directly"""
-    test_file_path = "test_test.py"
-    program_file_path = "E:/GreatRepository/Publishing/GitHubRepositories/Selenium-Tutorial/selenium_tutorial/helper/eve_wiki/eve_wiki_helper.py"
-    pytest_gen.generate_test_file_from_file(test_file_path, program_file_path)
-
-def run():
-    """Entry point for the runnable script."""
-    sys.exit(main(sys.argv[1:]))
-
-
-if __name__ == "__main__":
-    """main calls run()."""
-    run()
